1c639239f00b2973b0acc857f49f6a6691c7773c
Tero Laxström <tlax@neonpeons.com>
2025-05-05 22:04:15 +0300

    Adding fullscreen and quit

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+function love.load()
+  love.window.setFullscreen(true, "desktop")
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "f11" then
+    love.window.setFullscreen(true, "exclusive")
+  elseif key == "f12" then
+    love.window.setFullscreen(false)
+  elseif key == "escape" then
+    love.event.quit()
+  end
+end
+
 function love.draw()
   love.graphics.print('Hello World', 400, 300)
 end
